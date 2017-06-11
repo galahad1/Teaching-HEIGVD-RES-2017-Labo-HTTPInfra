1219421ba4c104781790d7f5e4c5d37fe5de16b2
Loan Lassalle <loan.lassalle@heig-vd.ch>
2017-06-11 17:20:36 +0200

    validate.sh

---
+++ validate.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+HOST="demo.res.ch"
+
+function show_help
+{
+	echo "$1 is not a valid argument"
+}
+
+function stop_all
+{
+	echo "Stopping all launched container"
+	docker kill $(docker ps -aq)
+	docker rm $(docker ps -aq)
+}
+
+function start_step1
+{
+	local port=9090
+    
+	echo "Starting step 1"
+	docker build -t res/apache_php ./docker-images/apache-php-image
+    docker run -d --name apache_static -p $port:80 res/apache_php
+	start "http://$HOST:$port"
+}
+
+function start_step2
+{    
+	local port=3000
+    
+	echo "Starting step 2"
+    docker build -t res/express_addresses ./docker-images/express-image
+	docker run -d --name express_dynamic -p $port:3000 res/express_addresses
+	start "http://$HOST:$port/"
+}
+
+## Main
+case $1 in
+	"step1" ) start_step1
+		;;
+	"step2" ) start_step2
+		;;
+	  * ) show_help
+esac
+
+read -p "Press any key to close all docker container ..."
+
+stop_all
