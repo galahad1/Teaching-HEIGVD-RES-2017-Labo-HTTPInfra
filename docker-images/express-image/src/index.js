eadb563daaca99e1f72303947d66ae880ea4ee17
loan.lassalle <loan.lassalle@heig-vd.ch>
2017-06-11 19:05:29 +0200

    HTTP Infrastructure ok

---
+++ docker-images/express-image/src/index.js
@@ -0,0 +1,35 @@
+var Chance = require('chance');
+var chance = new Chance();
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send(generateAddresses());
+});
+
+app.listen(3000, function () {
+    console.log('Accepting HTTP requests on port 3000.'); 
+});
+
+function generateAddresses() {
+	var numberOfAddresses = chance.integer({
+		min: 0,
+	    	max: 10
+    	});
+	console.log(numberOfAddresses);
+
+	var addresses = [];
+	for (var i = 0; i < numberOfAddresses; ++i) {
+		addresses.push({
+			street: chance.address(),
+			postal: chance.postal(),
+			city: chance.city(),
+			country: chance.country({
+				full: true
+			})
+		});
+	};
+	console.log(addresses);
+	return addresses;
+}
---
+++ validate.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+HOST="demo.res.ch"
+
+function stop_all {
+    echo "Stopping all launched container"
+    docker kill $(docker ps -aq)
+    docker rm $(docker ps -aq)
+}
+
+function validate
+{
+    local site_port=8080
+    local monitor_port=9090
+    
+    echo "Beginning validation of the entire infrastructure"
+    
+    docker build -t res/apache_php ./docker-images/apache-php-image
+    docker run -d --name apache_static res/apache_php
+    docker run -d res/apache_php
+    
+    docker build -t res/express_addresses ./docker-images/express-image
+    docker run -d --name express_dynamic res/express_addresses
+    docker run -d res/express_addresses
+
+    docker build -t res/traefik ./docker-images/traefik 
+    docker run -d --name traefik -p $monitor_port:8080 -p $site_port:80 -v /var/run/docker.sock:/var/run/docker.sock res/traefik
+
+    start "http://$HOST:$site_port/"
+    start "http://$HOST:$monitor_port/"
+    docker attach traefik
+}
+
+# Main
+validate
+read -p "Press any key to close all docker container ..."
+stop_all
