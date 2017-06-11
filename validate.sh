be29fe308e803c6d81f4341dc40cc77ffdfbfa71
Loan Lassalle <loan.lassalle@heig-vd.ch>
2017-06-11 18:55:21 +0200

    validate.sh error

--- validate.sh
+++ validate.sh
@@ -4,45 +4,45 @@ HOST="demo.res.ch"
 
 function show_help
 {
-	echo "$1 is not a valid argument"
+    echo "$1 is not a valid argument"
     echo "The parameter can be \"step1\" or \"step2\""
 
 function stop_all
 {
-	echo "Stopping all launched container"
-	docker kill $(docker ps -aq)
-	docker rm $(docker ps -aq)
+    echo "Stopping all launched container"
+    docker kill $(docker ps -aq)
+    docker rm $(docker ps -aq)
 }
 
 function start_step1
 {
-	local port=9090
+    local port=9090
     
-	echo "Starting step 1"
-	docker build -t res/apache_php ./docker-images/apache-php-image
+    echo "Starting step 1"
+    docker build -t res/apache_php ./docker-images/apache-php-image
     docker run -d --name apache_static -p $port:80 res/apache_php
-	start "http://$HOST:$port"
+    start "http://$HOST:$port"
 }
 
 function start_step2
 {    
-	local port=3000
+    local port=3000
     
-	echo "Starting step 2"
+    echo "Starting step 2"
     docker build -t res/express_addresses ./docker-images/express-image
-	docker run -d --name express_dynamic -p $port:3000 res/express_addresses
-	start "http://$HOST:$port/"
+    docker run -d --name express_dynamic -p $port:3000 res/express_addresses
+    start "http://$HOST:$port/"
 }
 
 ## Main
 error=false
 
 case $1 in
-	"step1" ) start_step1
-		;;
-	"step2" ) start_step2
-		;;
-	  * ) error=true; show_help
+    "step1" ) start_step1
+        ;;
+    "step2" ) start_step2
+        ;;
+      * ) error=true; show_help
 esac
 
 read -p "Press any key to close all docker container ..."
