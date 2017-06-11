e83482784bfbf7375a58ee913947cbce3d858b96
Loan Lassalle <loan.lassalle@heig-vd.ch>
2017-06-11 11:42:08 +0200

    Little mistakes

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -5,11 +5,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    	res.send(generateAddresses());
+    res.send(generateAddresses());
 });
 
 app.listen(3000, function () {
-    	console.log('Accepting HTTP requests on port 3000.'); 
+    console.log('Accepting HTTP requests on port 3000.'); 
 });
 
 function generateAddresses() {
