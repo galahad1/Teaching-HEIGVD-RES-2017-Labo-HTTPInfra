3d6a136cc86780baf721f138d86608b6a9ab07c1
Loan Lassalle <loan.lassalle@heig-vd.ch>
2017-06-11 18:54:03 +0200

    validate.sh error

--- validate.sh
+++ validate.sh
@@ -4,32 +4,34 @@ HOST="demo.res.ch"
 
 function show_help
 {
-	echo "$1 is not a valid argument"
+    echo "$1 is not a valid argument"
     echo "The parameter can be \"step1\""
 }
 
 function stop_all
 {
-	echo "Stopping all launched container"
-	docker kill $(docker ps -aq)
-	docker rm $(docker ps -aq)
+    echo "Stopping all launched container"
+    docker kill $(docker ps -aq)
+    docker rm $(docker ps -aq)
 }
 
 function start_step1
 {
-	local port=9090
+    local port=9090
     
-	echo "Starting step 1"
-	docker build -t res/apache_php ./docker-images/apache-php-image
+    echo "Starting step 1"
+    docker build -t res/apache_php ./docker-images/apache-php-image
     docker run -d --name apache_static -p $port:80 res/apache_php
-	start "http://$HOST:$port"
+    start "http://$HOST:$port"
 }
 
 ## Main
+error=false
+
 case $1 in
-	"step1" ) start_step1
-		;;
-	  * ) error=true; show_help
+    "step1" ) start_step1
+        ;;
+      * ) error=true; show_help
 esac
 
 read -p "Press any key to close all docker container ..."
